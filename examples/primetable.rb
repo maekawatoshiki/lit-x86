f257f6ac58f8779fdbc9d29a5c82cfab2a5182d1
maekawatoshiki <konndennsa@gmail.com>
2015-12-03 21:06:59 +0900

    edit examples/primetable.rb

--- examples/primetable.rb
+++ examples/primetable.rb
@@ -1,16 +1,21 @@
 $prime_list = 0
 
 def table(max)
+	count = max - 2
 	for i = 0, i < max, i++
 		prime_list[i] = 0
 	end
 	for i = 2, i * i < max, i++
 		if prime_list[i] == 0
 			for k = i * 2, k < max, k = k + i
-				prime_list[k] = 1
+				if prime_list[k] == 0
+					prime_list[k] = 1
+					count--
+				end
 			end
 		end
 	end
+	count
 end
 
 def isprime(n)
@@ -21,13 +26,13 @@ def isprime(n)
 	end
 end
 
-N = 1000000
+N = 10000000
 prime_list = Array(N)
 
-table(N)
+puts table(N)
 
-for i = 2, i < N, i++
-  if isprime(i) == 1
-    puts i, " is prime"
-  end
-end
+# for i = 2, i < N, i++
+#   if isprime(i) == 1
+#     puts i, " is prime"
+#   end
+# end
