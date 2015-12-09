410fddda47f6df7b4fe1b324259af78e431831d1
maekawatoshiki <konndennsa@gmail.com>
2015-12-09 18:12:57 +0900

    bug fix

--- examples/factlong.rb
+++ examples/factlong.rb
@@ -1,17 +1,21 @@
 $LEN = 10000
+$MAX = 10000
 
 def bigPrint(a)
 	for i = LEN - 1, i >= 0, i--
-		if a[i]; printf "%d", a[i]; end
+		if a[i]
+			printf "%d", a[i]
+		end
 	end 
 	puts ""
 end
 
 def bigSet(a, b)
 	c = b
+	for i = 0, i < LEN, i++; a[i] = 0; end
 	for i = 0, c, i++
-		a[i] = c % 1000000
-		c = c / 1000000
+		a[i] = c % MAX
+		c = c / MAX
 	end
 end
 
@@ -19,8 +23,8 @@ def bigAdd(a, b)
 	c = 0 
 	for i = 0, i < LEN or c, i++
 		c = c + a[i] + b[i]
-		a[i] = c % 1000000
-		c = c / 1000000
+		a[i] = c % MAX
+		c = c / MAX
 	end
 end
 
@@ -28,8 +32,8 @@ def bigMul(a, b)
 	c = 0
 	for i = 0, i < LEN or c, i++
 		c = c + a[i] * b
-		a[i] = c % 1000000
-		c = c / 1000000
+		a[i] = c % MAX
+		c = c / MAX
 	end
 end
 
