9d0a77d9295271e42a1dc02f611ca30a0736ca28
maekawatoshiki <konndennsa@gmail.com>
2015-12-20 18:52:38 +0900

    delete method 'mem_usage' from Sys module

--- lib/Sys_linux.c
+++ lib/Sys_linux.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/times.h>
-#include <sys/sysinfo.h>
-#include <sys/statvfs.h>
 #include <math.h>
-
-unsigned int Sys_mem_usage() {
-	struct sysinfo info;
-	sysinfo(&info);
-
-	unsigned long total = (info.totalram * info.mem_unit) / 1024,
-		free = (info.freeram * info.mem_unit ) / 1024;
-	
-	return (unsigned int)floor((double)free / total * 100.0);
-}
 
 void Sys_exit() {
 	exit(0);
