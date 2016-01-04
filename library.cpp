86553f18115e80a81b3f5d6d15640fcdbf5dbfed
maekawatoshiki <konndennsa@gmail.com>
2016-01-04 15:13:07 +0900

    refactoring library.cpp

--- library.cpp
+++ library.cpp
@@ -1,13 +1,11 @@
 #include "library.h"
-#include "lit.h"
 #include "util.h"
 
 LibraryList lib_list;
 
 LibraryList::~LibraryList() {
-	for(int i = 0; i < lib.size(); i++) {
+	for(int i = 0; i < lib.size(); i++) 
 		dlclose(lib[i].handle);
-	}
 }
 
 int LibraryList::append(std::string name) {
@@ -16,7 +14,8 @@ int LibraryList::append(std::string name) {
 		.no = lib.size(),
 		.handle = dlopen(("./lib/" + name + ".so").c_str(), RTLD_LAZY | RTLD_NOW)
 	};
-	if(l.handle == NULL) error("LitSystemError: cannot load library '%s'", name.c_str());
+	if(l.handle == NULL)
+		error("LitSystemError: cannot load library '%s'", name.c_str());
 	lib.push_back(l);
 	return lib.size() - 1;
 }
@@ -27,15 +26,14 @@ bool LibraryList::is(std::string mod_name) {
 
 lib_t *LibraryList::get(std::string name) {
 	for(int i = 0; i < lib.size(); i++) {
-		if(lib[i].name == name) {
-			return &lib[i];
-		}
+		if(lib[i].name == name) return &lib[i];
 	}
 	return NULL;
 }
 
 uint32_t LibraryList::call(std::string name, std::string mod_name) {
 	void *function = dlsym(get(mod_name)->handle, (mod_name + "_" + name).c_str());
-	if(function == NULL) error("LitSystemError: cannot load function '%s'", (mod_name + "_" + name).c_str());
+	if(function == NULL) 
+		error("LitSystemError: cannot load function '%s'", (mod_name + "_" + name).c_str());
 	return (uint32_t)function;
 }
