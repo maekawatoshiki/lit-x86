2768812718d428c84e1d41fb6137f35b895f60a1
maekawatoshiki <konndennsa@gmail.com>
2016-01-25 23:17:58 +0900

    bug fix

--- expr.cpp
+++ expr.cpp
@@ -223,13 +223,13 @@ int Parser::expr_primary(ExprType &et) {
 			error("error: %d: expected expression ')'", tok.get().nline);
 	} else if(!make_array(et)) error("error: %d: invalid expression", tok.get().nline);
 
-	if(tok.skip(".")) {
+	while(tok.skip(".")) {
 		name = tok.next().val;
 		func_t *function = funcs.get(name, mod_name);
 		if(function == NULL) 
 			function = funcs.get(name, module);
 		if(function == NULL) error("function not found");
-		ntv.genas("push eax");
+		if(function->params > 0) ntv.genas("push eax");
 		if(HAS_PARAMS_FUNC) {
 			for(size_t i = 0; i < function->params - 1; i++) {
 				expr_entry();
@@ -238,7 +238,7 @@ int Parser::expr_primary(ExprType &et) {
 			}
 		} tok.skip(")");
 		ntv.gencode(0xe8); ntv.gencode_int32(0xFFFFFFFF - (ntv.count - function->address) - 3); // call func
-		ntv.genas("add esp %d", function->params * ADDR_SIZE + ADDR_SIZE);
+		ntv.genas("add esp %d", function->params * ADDR_SIZE);
 	} 
 
 	while(is_index()) make_index(et);
