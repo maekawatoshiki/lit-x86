45ad4a14e11a58203f7e6bad14c0a382150901e1
maekawatoshiki <konndennsa@gmail.com>
2016-04-15 21:45:24 +0900

    edit library 'File' to support FileIO

--- lib/File.c
+++ lib/File.c
@@ -5,22 +5,22 @@ FILE *File_new() {
 	return malloc(sizeof(FILE *));
 }
 
-FILE *File_open(int *fp, char *name, char *mode) {
-	*fp = (int)fopen(name, mode);
+int File_open(char *name, char *mode) {
+	return (int)fopen(name, mode);
 }
 
-void File_write(int *fp, char *str) {
-	fprintf((FILE*)*fp, "%s", str);
+void File_write(int fp, char *str) {
+	fprintf((FILE*)fp, "%s", str);
 }
 
-char *File_read(int *fp) {
-	fseek((FILE*)*fp, 0, SEEK_END);
-	size_t sz = ftell((FILE*)*fp); fseek((FILE*)*fp, 0, SEEK_SET);
+char *File_read(int fp) {
+	fseek((FILE*)fp, 0, SEEK_END);
+	size_t sz = ftell((FILE*)fp); fseek((FILE*)fp, 0, SEEK_SET);
 	char *str = malloc(sz + 1);
-	fread(str, sz, 1, (FILE*)*fp);
+	fread(str, sz, 1, (FILE*)fp);
 	return str;
 }
 
-void File_close(int *fp) {
-	fclose((FILE*)*fp);
+void File_close(int fp) {
+	fclose((FILE*)fp);
 }
---
+++ lib/File.rb
@@ -0,0 +1,6 @@
+lib File
+	proto File_open name:string mode:string
+	proto File_write id str:string
+	proto File_read id:int : string
+	proto File_close id
+end
