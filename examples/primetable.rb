6e441f11929d8e67132cf2ff2b68619daaa6fa78
maekawatoshiki <konndennsa@gmail.com>
2016-04-15 18:17:32 +0900

    edit example

--- examples/primetable.rb
+++ examples/primetable.rb
@@ -1,11 +1,11 @@
 def table prime_list:int[] max
 	count = max - 2
-	for i = 0, i < max, i = i + 1
+	for i in 0..max-1
 		prime_list[i] = 0
 	end
-	for i = 2, i * i < max, i = i + 1
+	for i = 2, i * i < max, i += 1
 		if prime_list[i] == 0
-			for k = i * 2, k < max, k = k + i
+			for k = i * 2, k < max, k += i
 				if prime_list[k] == 0
 					prime_list[k] = 1
 					count = count - 1
@@ -18,10 +18,4 @@ end
 
 N = 10000000
 prime_list = new N int
-GC
 puts "found " table(prime_list N) " primes between 2 and " N
-# for i = 2, i < N, i = i + 1
-# 	if prime_list[i] == 0
-# 		puts(i, " is prime")
-# 	end
-# end
