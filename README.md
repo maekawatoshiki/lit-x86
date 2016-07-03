1032a0b52a94d76298e3824f46199c11ed5e1f31
maekawatoshiki <konndennsa@gmail.com>
2016-07-03 17:10:09 +0900

    update make test script

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# Introduction
+# Lit
+[![Build Status](https://travis-ci.org/maekawatoshiki/lit-x86.svg?branch=llvm)](https://travis-ci.org/maekawatoshiki/lit-x86)
+
 - Lit is a programming language designed fast & light weight with LLVM
 
-# Language Wiki
+## Language Wiki
 Please look at Wiki.
 
-# Build
+## Build
 - Lit generates native codes with LLVM.
 - So when you build, need LLVM and Clang.
 
@@ -16,8 +18,8 @@ $ make
 $ ./lit -h # -h option shows helps
 ```
 
-# Notes
+## Notes
 - 2015/7/22 : First Commit
 
-# License
+## License
 - BSD
--- test/test.sh
+++ test/test.sh
@@ -3,19 +3,39 @@
 function check() {
 	if [[ $($1) = $($2) ]] ; then
 		printf "$1 ... \e[32mOK\e[00m\n"
+		return 0
 	else
 		printf "$1 ... \e[31mNO\e[00m\n"
+		return 1
 	fi
 }
 
-check './lit ./test/io.rb' 'cat ./test/io' &
+check "./lit ./test/io.rb" "cat ./test/io" &
+pid1=$!
 check './lit ./examples/fibo.rb' 'echo 165580141' &
+pid2=$!
 check './lit ./examples/pi.rb' 'cat test/pi' &
+pid3=$!
 check './lit ./examples/primetable.rb' "cat ./test/primetable" &
+pid4=$!
 check './lit ./examples/vararg.rb' 'echo 31' &
+pid5=$!
 check './lit ./examples/funccall.rb' 'echo 4' &
+pid6=$!
 check './lit ./examples/math.rb' 'cat test/math' &
+pid7=$!
 check './lit ./test/require.rb' 'echo 2' &
+pid8=$!
 check './lit ./examples/array.rb' 'echo 12345' &
-wait
-exit 0
+pid9=$!
+status_sum=0
+wait $pid1; status_sum=`expr $status_sum + $?`
+wait $pid2; status_sum=`expr $status_sum + $?`
+wait $pid3; status_sum=`expr $status_sum + $?`
+wait $pid4; status_sum=`expr $status_sum + $?`
+wait $pid5; status_sum=`expr $status_sum + $?`
+wait $pid6; status_sum=`expr $status_sum + $?`
+wait $pid7; status_sum=`expr $status_sum + $?`
+wait $pid8; status_sum=`expr $status_sum + $?`
+wait $pid9; status_sum=`expr $status_sum + $?`
+exit $status_sum
