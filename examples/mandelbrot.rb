133137da96203d979ff97a42603799ab36d9f6f6
uint256_t <konndennsa@gmail.com>
2016-08-21 19:11:25 +0900

    update an example

--- examples/mandelbrot.rb
+++ examples/mandelbrot.rb
@@ -10,7 +10,7 @@ def m c_x:double, c_y:double, n
 		x_n_1 = x_n*x_n - y_n*y_n + c_x
 		y_n_1 = 2.0 * x_n * y_n + c_y
 		if x_n_1*x_n_1 + y_n_1*y_n_1 > 4.0 
-			return n
+			return x_n_1*x_n_1 + y_n_1*y_n_1
 		else
 			x_n = x_n_1
 			y_n = y_n_1
@@ -18,24 +18,31 @@ def m c_x:double, c_y:double, n
 	end
 	0
 end
+def print_density d
+	if d > 8
+		print ' '
+	elsif d > 4
+		print '.'
+	elsif d > 2
+		print '*'
+	else
+		print '+'
+	end
+end
 
 $x_max = 1.0
-$x_min = 0.0-2
+$x_min = -2.0
 $y_max = 1.0
-$y_min = 0.0-1
-$dx = 0.1
-$dy = 0.1
+$y_min = -1.0
+$dx = 0.06
+$dy = 0.06
 
 x = 0.0
 y = 0.0
 x_length = ($x_max - $x_min) / $dx;
 for y = $y_max, y > $y_min, y -= $dy
 	for x = $x_min, x < $x_max, x += $dx 
-		if m(x, y, 300) == 0 
-			print "*"
-		else
-			print " "
-		end
+		print_density m(x, y, 300)
 	end
 	puts
 end
